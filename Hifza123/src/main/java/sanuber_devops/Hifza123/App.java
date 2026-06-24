9dc22715bf37646f7eeccb6bd2c510e7eac319ad
Hifza <hifzasanuberbs@gmail.com>
2026-06-24 18:59:50 +0530

    first commit

---
+++ Hifza123/src/main/java/sanuber_devops/Hifza123/App.java
@@ -0,0 +1,13 @@
+package sanuber_devops.Hifza123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
